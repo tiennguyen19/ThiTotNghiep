0f58d995a7fc6bd857fd873880e6da7b09ead30b
tiennguyen19 <triteppy2003@gmail.com>
2022-09-14 18:22:53 +0700

    Tạo new branch

--- src/main/java/Bai1/BigDog.java
+++ src/main/java/Bai1/BigDog.java
@@ -38,6 +38,6 @@ public class BigDog extends SmallDog{
 
     @Override
     public String makeSound() {
-        return ("woof woof woof");
+        return ("woof woof woof branch tien2");
     }
 }
